d5b24ba1bd894c1ef85600202eaa7744e4c3454d
oscar <denfu.oscar.dev@gmail.com>
2024-12-02 14:55:04 +0000

    chore: Add types

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { LoginForm } from "./components/login-form";
 
-export default function Page() {
+export default async function Page() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
       <LoginForm />
---
+++ types.d.ts
@@ -0,0 +1,26 @@
+import { AxiosHeaders } from "axios";
+
+type ServicesType<SData = unknown> = {
+  method: `GET` | `POST` | `PUT` | `DELETE`;
+  url: string;
+  data?: SData;
+  headers?: AxiosHeaders;
+};
+
+type SuccessResponseType<Data = unknown> = {
+  status: "success";
+  status_code: 200;
+  message: string;
+  data: Data; // The actual data returned by the API
+};
+
+type ErrorResponseType<Error = unknown> = {
+  status: "error";
+  status_code: 400 | 401 | 403 | 404 | 500;
+  message: string;
+  data: Error; // The actual data returned by the API
+};
+
+interface GenericType {
+  success: boolean;
+}
