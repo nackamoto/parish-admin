09df166b1331d44eb5a07763d5eea2447083f843
oscar <denfu.oscar.dev@gmail.com>
2024-12-02 15:17:50 +0000

    feat: Add forgot password

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ForgotPassword } from "./components/forgot-password";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <ForgotPassword />
+    </div>
+  );
+}
