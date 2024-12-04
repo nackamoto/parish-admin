5e369cd85c8b3c3b10ee8f389c15b51be59a7a60
oscar <denfu.oscar.dev@gmail.com>
2024-12-04 12:14:55 +0000

    feat: Add toast component

--- app/components/app-layout.tsx
+++ app/components/app-layout.tsx
@@ -1,11 +1,8 @@
 import { AppSidebar } from "@/app/components/app-sidebar";
-import { Separator } from "@/app/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/app/components/ui/sidebar";
+// import { Separator } from "@/app/components/ui/separator";
+import { SidebarInset, SidebarProvider } from "@/app/components/ui/sidebar";
 import { ReactNode } from "react";
+import { Toaster } from "./ui/toaster";
 
 export default async function AppLayout({ children }: { children: ReactNode }) {
   return (
@@ -27,6 +24,7 @@ export default async function AppLayout({ children }: { children: ReactNode }) {
           </div>
           <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
         </div> */}
+        <Toaster />
       </SidebarInset>
     </SidebarProvider>
   );
