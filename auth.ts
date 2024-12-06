3c34568c7006ddb7dd2af54afc681194931b5db1
oscar <denfu.oscar.dev@gmail.com>
2024-12-06 11:43:27 +0000

    feat: Add token expiration

--- auth.ts
+++ auth.ts
@@ -3,4 +3,10 @@ import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
+  secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    // let's set the expiration to 24 hours after the user logs in
+    maxAge: 24 * 60 * 60,
+  },
 });
