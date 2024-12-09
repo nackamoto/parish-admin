875f88d850324584b66751a7be0f51f5633948e1
oscar <denfu.oscar.dev@gmail.com>
2024-12-09 15:22:46 +0000

    update session providers

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,17 @@
+import { auth } from "@/auth";
 import AppLayout from "../components/app-layout";
+import QueryProviders from "../components/query-provider";
+import { NextAuthSessionProvider } from "../components/session-provider";
 
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <AppLayout>{children}</AppLayout>;
+  const session = await auth();
+  return (
+    <NextAuthSessionProvider session={session}>
+      <QueryProviders>
+        <AppLayout>{children}</AppLayout>
+      </QueryProviders>
+    </NextAuthSessionProvider>
+  );
 }
--- app/components/session-provider.tsx
+++ app/components/session-provider.tsx
@@ -1,11 +1,17 @@
 "use client";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { PropsWithChildren, ReactNode } from "react";
 
 export function NextAuthSessionProvider(props: SessionProps) {
   return (
-    <SessionProvider refetchOnWindowFocus>{props.children}</SessionProvider>
+    <SessionProvider refetchOnWindowFocus session={props.session}>
+      {props.children}
+    </SessionProvider>
   );
 }
 
-type SessionProps = PropsWithChildren<{ children: ReactNode }>;
+type SessionProps = PropsWithChildren<{
+  children: ReactNode;
+  session: Session | null;
+}>;
